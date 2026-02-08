1d37d416d9755e30679302dad6f746621c3e39a2
SumonMohammad <miamohammadsumon10@gmail.com>
2026-02-08 07:59:00 +0600

    added ci pipeline

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -4,9 +4,7 @@ import { expect } from '@playwright/test';
 test("Upload transcript file and analyze", async ({dashboardPage, page})=>{  
     await expect(page).toHaveURL(/dashboard/)
     await dashboardPage.uploadTranscriptFile();
-
-
-    
+  
 })
 
 
