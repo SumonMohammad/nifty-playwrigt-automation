003b6830e5230f1e37d7dffffe8753a213a0134f
SumonMohammad <miamohammadsumon10@gmail.com>
2026-02-08 09:51:06 +0600

    added secrets to github again

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -1,11 +1,10 @@
 import { test} from '../src/fixtures/base-fixture';
 import { expect } from '@playwright/test';
 
-test("Upload transcript file and analyze", async ({dashboardPage, page})=>{  
-    await expect(page).toHaveURL(/dashboard/)
-    await dashboardPage.uploadTranscriptFile();
-  
-})
+// test("Upload transcript file and analyze", async ({dashboardPage, page})=>{  
+//     await expect(page).toHaveURL(/dashboard/)
+//     await dashboardPage.uploadTranscriptFile(); 
+// })
 
 
 
--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:5004/');
-  await page.getByRole('link', { name: 'Sign In', exact: true }).click();
-  await page.getByRole('link', { name: 'Sign in with Zoom' }).click();
-});
-await page.getByRole('link', { name: 'Sign in with Zoom' }).click();
